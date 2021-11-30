e7c11974c33c124b9a578f431f45477c9860c4e7
Nibro87 <71063030+Nibro87@users.noreply.github.com>
2021-11-30 19:30:33 +0100

    update

--- src/ResultList.js
+++ src/ResultList.js
@@ -4,6 +4,7 @@ import ChildComponent from "./Child";
 const { Component } = require("react");
 
 
+const API_URL = "http://localhost:8080/CA2_war_exploded";
 
 
  class ResultList extends Component{
@@ -23,6 +24,13 @@ const { Component } = require("react");
     }
    
 
+    shareArticle = (data) => {
+    
+    axios.post(API_URL + "/api/info/share",data)
+    .then(()=>{})
+    
+    }
+
 
     setNews=(data)=> {
         console.log(data)
@@ -40,7 +48,7 @@ const foundArticles = this.state.news.length != 0
 return(
             <>
         <div>
-            <ChildComponent parentCallback = {this.setNews}/>
+            <ChildComponent parentCallback = {this.setNews} />
       
             <>
 
@@ -48,16 +56,21 @@ return(
 {foundArticles ? this.state.news.map(article =>(
  
 <Card style={{ width: '40rem' }}>
-<Card.Img variant="top" src={article.urlToImage} />
+
+<Card.Img variant="top"  src={article.urlToImage} />
 <Card.Body>
-  <Card.Title><a target="_blank" href= {article.url}>{article.title}</a></Card.Title>
+  <Card.Title><a target="_blank"  href= {article.url}>{article.title}</a></Card.Title>
   <Card.Subtitle className="mb-2 text-muted">{article.publishedAt}</Card.Subtitle>
-  <Card.Text>
-    {article.description}
+  <Card.Text> 
+   {article.description}
+   </Card.Text>
    
-  </Card.Text>
- 
 </Card.Body>
+
+<button onClick={() =>{this.shareArticle(article)}} variant="primary">Share</button>
+
+
+
 </Card>
 
 )) : <p>no articles</p>}
